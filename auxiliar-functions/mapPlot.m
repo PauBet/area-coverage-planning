92c46f1812e7e8931974f730e1e2085631bbd6ea
PauBet <betriuroure@gmail.com>
2023-04-03 11:52:18 +0200

    mapPlot minor update

--- auxiliar-functions/mapPlot.m
+++ auxiliar-functions/mapPlot.m
@@ -4,18 +4,19 @@ xtick = -180:60:180;
 ytick = -90:30:90;
 
 W = scrsz(3); L = scrsz(4);
-figure('Position',[100 100 W*0.8 L*0.7]);
+figure('Position',[0.4307*W 0.3144*L W*0.6 L*0.5]);
 ax = axes;
-title('Topography Map','FontSize',20)
-hold on; grid on; box on;
+title('Coverage Map')
+hold on; grid on; box on; axis equal;
 I = flip(imread(filename));
 imagesc([-180, 180], [-90, 90], I)
 colormap('gray');
-set(gca, 'XTick', xtick, 'YTick', ytick, 'Layer', 'top', 'GridColor', 'w', 'GridAlpha', 1, 'GridLineStyle', ':');
-set(gca, 'FontSize', 15)
+set(gca, 'XTick', xtick, 'YTick', ytick, 'Layer', 'top', ...
+    'GridColor', 'w', 'GridAlpha', 1, 'GridLineStyle', ':');
+set(gca, 'FontSize', 20)
 xlim([min(xtick) max(xtick)]);
 ylim([min(ytick) max(ytick)]);
-xlabel('Longitude [°]')
-ylabel('Latitude [°]')
+xlabel('Planetocentric longitude [°]')
+ylabel('Planetocentric latitude [°]')
 
 end
