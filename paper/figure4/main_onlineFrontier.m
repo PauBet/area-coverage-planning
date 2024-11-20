1f5ad14226282fb077fc77a7ca9c386840bf3acf
PauBet <betriuroure@gmail.com>
2024-11-20 16:21:41 +0100

    Updated main online frontier for paper figure 4

--- paper/figure4/main_onlineFrontier.m
+++ paper/figure4/main_onlineFrontier.m
@@ -10,11 +10,12 @@ roiname(isspace(roiname)) = [];
 name = ['post_process_',roiname];
 
 % Define program iteration info
-td = cspice_str2et('1998 MAR 29 12:10:00.000 TDB'); % initial observation
+td = cspice_str2et('1998 MAR 29 12:20:00.000 TDB'); % initial observation
 % time
-N = 15; % number of iterations
+N = 90; % number of iterations
 mkspan = zeros(1, N); % initialize mkspan array
 coverage = zeros(1, N); % initialize coverage array
+overlap  = zeros(1, N); % initialize overlap array
 nfp = zeros(1, N); % initialize number of acquisitions array
 step = 30; % time step in [sec]
 
@@ -30,7 +31,7 @@ for i=1:N
     % leg.String(end) = [];
     
     % Get coverage, number of acquisitions and makespan
-    coverage(i) = roicoverage(target, roi, fplist);
+    [coverage(i), overlap(i)] = roicoverage(target, roi, fplist);
     mkspan(i) = fplist(end).t + tcadence - fplist(1).t;
     nfp(i) = length(fplist);
     
